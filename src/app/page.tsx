6408812bde05500f13e55cf8431cb0864194226f
Justin <justinzhou.web3@gmail.com>
2025-01-17 11:44:24 -0500

    Refactor Home component for improved search functionality and code consistency
    - Cleaned up imports and standardized formatting in the Home component.
    - Enhanced the search input placeholder for better user guidance.
    - Improved the handling of loading states and filtered results for Kols and Tokens.
    - Refactored the rendering logic for displaying filtered results, ensuring clearer structure and readability.
    
    These changes enhance the user experience by providing a more intuitive search interface and improving code maintainability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ import { useEffect, useState } from "react";
 import { getTickers, getKols } from "@/api";
 import NextLink from "next/link";
 import { Divider } from "@nextui-org/react";
-import useDebounce from "@/hooks/useDebounce"
+import useDebounce from "@/hooks/useDebounce";
 import Loading from "@/components/Loading";
-import { shortenAddress } from "@/util/formatter"
-import { TokenList } from "@/types"
+import { shortenAddress } from "@/util/formatter";
+import { TokenList } from "@/types";
 
 export default function Home() {
   const [kolsList, setKolsList] = useState<string[]>([]);
@@ -19,7 +19,7 @@ export default function Home() {
 
   const [filteredKols, setFilteredKols] = useState<string[]>([]);
   const [filteredTokens, setFilteredTokens] = useState<TokenList[]>([]);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     // 从缓存中读取数据
@@ -36,25 +36,31 @@ export default function Home() {
         const [kols, ticks] = await Promise.all([getKols(), getTickers()]);
         setKolsList(kols.tickers);
 
-        const filterList = Array.from(new Map(ticks.tickers.map(([name, address, num]) => [address, { name, address, num }])).values())
+        const filterList = Array.from(
+          new Map(
+            ticks.tickers.map(([name, address, num]) => [
+              address,
+              { name, address, num },
+            ])
+          ).values()
+        );
 
-        setTokenList(filterList)
+        setTokenList(filterList);
         // 缓存到 localStorage
         localStorage.setItem("kolsList", JSON.stringify(kols.tickers));
         localStorage.setItem("tokenList", JSON.stringify(filterList));
       };
       getTickersApi();
     }
-  }, [])
-
+  }, []);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setSearchText(value);
-  }
+  };
 
   const checkfilter = useDebounce((text) => {
-    setLoading(true)
+    setLoading(true);
 
     const kolsFiltered = kolsList.filter((item) =>
       item.toLowerCase().includes(text.toLowerCase())
@@ -64,16 +70,15 @@ export default function Home() {
       item.name.toLowerCase().includes(text.toLowerCase())
     );
     setTimeout(() => {
-      setLoading(false)
+      setLoading(false);
       setFilteredKols(kolsFiltered);
       setFilteredTokens(tokenFiltered);
     }, 1000);
-
-  }, 1000)
+  }, 1000);
 
   useEffect(() => {
-    checkfilter(searchText)
-  }, [searchText])
+    checkfilter(searchText);
+  }, [searchText]);
 
   return (
     <div
@@ -92,7 +97,13 @@ export default function Home() {
         className="relative h-[262px] flex-col items-center flex rounded-full border-1 border-[rgba(255,255,255,.1)]"
         style={{ width: "1160px" }}
       >
-        <Image className="mt-10" src={"/title.svg"} width={964} height={192} alt="" />
+        <Image
+          className="mt-10"
+          src={"/title.svg"}
+          width={964}
+          height={192}
+          alt=""
+        />
         <div
           className={`top-[170px] absolute flex flex-col w-[3/4]  bg-[#292543] rounded-[40px] ${searchText}?"h-[388px]":"h-[0]" `}
           style={{ width: "70%" }}
@@ -100,51 +111,69 @@ export default function Home() {
           {/* 输入框 */}
           <input
             type="text"
-            placeholder="Type Your Message"
+            placeholder="Search your interest Kol or Token"
             className="border-[#7676E0] border-1 w-full text-center py-4 text-lg text-black bg-white from-gray-800 to-gray-900 rounded-full outline-none placeholder-[#8181E5]"
             value={searchText}
             onChange={handleInputChange}
           />
-          {searchText &&
-            <div className="p-5 px-6 overflow-y-auto flex flex-col" style={{ height: "300px" }}>
-              {
-                loading ? <Loading></Loading> :
-                  <div className="overflow-y-auto flex flex-col gap-4">
-                    <div className="flex flex-col gap-4">
-                      <span className="text-[#ccc]">Tokens</span>
-                      <div className="flex flex-col gap-2">
-                        {
-                          filteredTokens.length > 0 ? filteredTokens.map((item, index) => {
-                            return <NextLink href={`/token/${item.name.toLowerCase()}`} key={index}>
+          {searchText && (
+            <div
+              className="p-5 px-6 overflow-y-auto flex flex-col"
+              style={{ height: "300px" }}
+            >
+              {loading ? (
+                <Loading></Loading>
+              ) : (
+                <div className="overflow-y-auto flex flex-col gap-4">
+                  <div className="flex flex-col gap-4">
+                    <span className="text-[#ccc]">Tokens</span>
+                    <div className="flex flex-col gap-2">
+                      {filteredTokens.length > 0 ? (
+                        filteredTokens.map((item, index) => {
+                          return (
+                            <NextLink
+                              href={`/token/${item.name.toLowerCase()}`}
+                              key={index}
+                            >
                               <div className="flex gap-2 items-center">
                                 {/* <Image src={"/token.svg"} width={28} height={28} alt=""></Image> */}
-                                  <span className="font-bold">{item.name}</span>
-                                  <span className="text-xs text-zinc-400 opacity-2">{shortenAddress(item.address)}</span>
+                                <span className="font-bold">{item.name}</span>
+                                <span className="text-xs text-zinc-400 opacity-2">
+                                  {shortenAddress(item.address)}
+                                </span>
                               </div>
                             </NextLink>
-                          }) : <div>No results found in Kols</div>
-                        }</div>
+                          );
+                        })
+                      ) : (
+                        <div>No results found in Kols</div>
+                      )}
                     </div>
-                    <Divider></Divider>
-                    <div className="flex flex-col gap-4">
-                      <span className="text-[#ccc]">Kol</span>
-                      <div className="flex flex-col gap-2">
-                        {
-                          filteredKols.length > 0 ? filteredKols.map((item, index) => {
-                            return <NextLink href={`/detail/${item}`} key={index}>
+                  </div>
+                  <Divider></Divider>
+                  <div className="flex flex-col gap-4">
+                    <span className="text-[#ccc]">Kol</span>
+                    <div className="flex flex-col gap-2">
+                      {filteredKols.length > 0 ? (
+                        filteredKols.map((item, index) => {
+                          return (
+                            <NextLink href={`/detail/${item}`} key={index}>
                               <div className="flex gap-2">
                                 {/* <Image src={"/kol.svg"} width={28} height={28} alt=""></Image> */}
                                 <span className="font-bold">{item}</span>
                               </div>
                             </NextLink>
-                          }) : <div>No results found in kol</div>
-                        }</div>
+                          );
+                        })
+                      ) : (
+                        <div>No results found in kol</div>
+                      )}
                     </div>
                   </div>
-              }
+                </div>
+              )}
             </div>
-          }
-
+          )}
 
           {/* <NextLink href="detail/1243" className="absolute" style={{ right: 0 }}>
           <Button className="rounded-full flex justify-center items-center" style={{ width: "56px", height: "56px", backgroundColor: "#8181E5"}}>
